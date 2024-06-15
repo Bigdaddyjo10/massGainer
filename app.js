3fb5cbf801ac68725c1974271e4cc900ba2a45ce
jo <josehehall98@gmail.com>
2024-06-14 23:30:03 -0600

    link all Buttons and did the logic for them

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const upgrades = [
     },
     {
         name: 'STEROIDS',
-        cost: 15,
+        cost: 50,
         gains: 1000,
         ingested: false
     }
@@ -23,10 +23,13 @@ let gainMeter = 0
 
 const massMeterElement = document.getElementById('massCounter')
 
+
+
+
 function buyingProtein() {
     const protein = upgrades.find((upgrade) => upgrade.name == 'protein shakes')
     if (gainMeter > protein.cost) {
-        console.log("protein");
+
         protein.ingested = true
         gainMeter += protein.gains
         massMeterElement.innerText = gainMeter
@@ -39,60 +42,62 @@ function buyingHormone() {
 
     const hormone = upgrades.find((upgrade) => upgrade.name == 'growth hormone')
     if (gainMeter > hormone.cost) {
-        console.log("hormone");
+
         hormone.ingested = true
         gainMeter += hormone.gains
         massMeterElement.innerText = gainMeter
 
     }
-    console.log(gainMeter)
+    setTimeout(() => {
+        upgrades.filter((upgrade) => upgrade.name == "growth hormone")
+        upgrades.forEach((upgrade) => {
+            upgrade.ingested = false
+        })
+    }, 3000)
 
 }
 function buyingSTEROIDS() {
     // const massMeterElement = document.getElementById('massCounter')
     const STEROIDS = upgrades.find((upgrade) => upgrade.name == "STEROIDS")
     if (gainMeter > STEROIDS.cost) {
-        console.log("STEROIDS");
+
         STEROIDS.ingested = true
         gainMeter += STEROIDS.gains
         massMeterElement.innerText = gainMeter
     }
+    setTimeout(() => {
+        upgrades.forEach((upgrade) => {
+            upgrade.ingested = false
+        })
+    }, 1000)
+
 }
 
 function addUpGrade() {
     upgrades.forEach((upgrade) => {
         if (upgrade.name == 'protein shakes' && upgrade.ingested == true) {
-            console.log("power");
             gainMeter += upgrade.gains
             gainMeter++
             // massMeterElement.innerText += gainMeter
 
         }
         if (upgrade.name == 'growth hormone' && upgrade.ingested == true) {
-            console.log("power");
             gainMeter += upgrade.gains
             gainMeter++
             // massMeterElement.innerText += gainMeter
 
         }
         if (upgrade.name == 'STEROIDS' && upgrade.ingested == true) {
-            console.log("STEROIDS");
             gainMeter += upgrade.gains
             gainMeter++
         }
         // const massMeterElement = document.getElementById('massCounter')
         massMeterElement.innerText = gainMeter
-
     })
-
 }
 function growMass() {
     // const massMeterElement = document.getElementById('massCounter')
     gainMeter++
 }
 
-
-// upgrades.forEach((upgrade) => {
-//     upgrade.ingested == true
-// })
-setInterval(addUpGrade, 100)
+setInterval(addUpGrade, 1000)
